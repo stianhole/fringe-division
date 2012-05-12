eff7a1f2148d2ff369344126f8c6fc5f0ed76974
Stian Hole <stian.hole@gmail.com>
2012-05-12 12:54:40 +0200

    Removed webview, readded widget

--- Saldo/gen/me/kaptindandy/saldo/R.java
+++ Saldo/gen/me/kaptindandy/saldo/R.java
@@ -15,13 +15,11 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int Corners=0x7f070002;
-        public static final int button=0x7f070001;
-        public static final int livemap=0x7f070006;
-        public static final int sendtest=0x7f070004;
-        public static final int settings=0x7f070003;
-        public static final int showprefs=0x7f070005;
-        public static final int widget1label=0x7f070000;
+        public static final int Corners=0x7f070001;
+        public static final int button=0x7f070000;
+        public static final int sendtest=0x7f070003;
+        public static final int settings=0x7f070002;
+        public static final int showprefs=0x7f070004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
---
+++ Saldo/src/me/kaptindandy/saldo/SaldoWidget.java
@@ -0,0 +1,41 @@
+package me.kaptindandy.saldo;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+public class SaldoWidget extends AppWidgetProvider {
+	
+	  DateFormat df = new SimpleDateFormat("hh:mm:ss");
+	  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+	    final int N = appWidgetIds.length;
+	    Log.i("ExampleWidget",  "Updating widgets " + Arrays.asList(appWidgetIds));
+	    // Perform this loop procedure for each App Widget that belongs to this
+	    // provider
+	    for (int i = 0; i < N; i++) {
+	      int appWidgetId = appWidgetIds[i];
+	      // Create an Intent to launch ExampleActivity
+	      Intent intent = new Intent(context, SaldoActivity.class);
+	      PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+	      // Get the layout for the App Widget and attach an on-click listener
+	      // to the button
+	      RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget1);
+	      views.setOnClickPendingIntent(R.id.button, pendingIntent);
+	      // To update a label
+	      //views.setTextViewText(R.id.widget1label, df.format(new Date()));
+	      // Tell the AppWidgetManager to perform an update on the current app
+	      // widget
+	      appWidgetManager.updateAppWidget(appWidgetId, views);
+	    }
+	  }
+
+
+}
